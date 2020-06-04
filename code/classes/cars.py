398f3367d878e5b3d9aa15f1dd8f07bef8906785
Nina Borsboom <njborsboom@hotmail.com>
2020-06-04 22:16:04 +0200

    added get_name and get_move_count

--- code/classes/cars.py
+++ code/classes/cars.py
@@ -8,6 +8,7 @@ class Car:
         self.length = int(length)
 
         self.position = {}
+        self.move_count = 0
 
     def is_red(self):
         # check if this car is the red car
@@ -28,8 +29,18 @@ class Car:
             # in case there is an error in the loaded orientation
             return False
 
+        self.move_count += 1
+
+    def get_name(self):
+        # returns the name of the car
+        return self.name
+
     def get_position(self):
         # returns position of car on the board
         self.position["row"] = self.row
         self.position["col"] = self.col
         return self.position
+
+    def get_move_count(self):
+        # returns the number of times a car has been moved
+        return self.move_count
