bcc885b3b54d4f1a1192e0cfa3f67cc92e24bf5c
jazzlikefruit <shewen_dave@hotmail.com>
2020-06-05 00:24:03 +0200

    Board.py loads cars

--- code/classes/board.py
+++ code/classes/board.py
@@ -1,15 +1,25 @@
+import csv
+from cars import Car
+
+
 class Board():
     def __init__(self, source_file):
         self.cars = self.load_cars(source_file)
 
-
     def load_cars(self, datafile):
-        
-        # Open datafile 
+        """
+        Load the cars of a board 
+        """
+        # Open datafile
         with open(datafile, 'r') as file:
             cars = {}
-            reader = csv.DictReader(in_file)
+            reader = csv.DictReader(file)
             for row in reader:
-                cars[row['name']]
+                cars[row['car']] = Car(
+                    row['car'], row['orientation'], row['row'], row['col'], row['length'])
 
-    
+        return cars
+
+
+if __name__ == '__main__':
+    print(Board("data/Rushhour6x6_1.csv").__str__())
