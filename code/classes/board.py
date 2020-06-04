f8f9d37bc9c3f6879a10f643766ff77267947987
root <root@Shewen-pc.localdomain>
2020-06-04 22:31:02 +0200

    work on board

--- code/classes/board.py
+++ code/classes/board.py
@@ -1,13 +1,15 @@
 class Board():
-    def __init__(self, game):
-        self.cars = {}
+    def __init__(self, source_file):
+        self.cars = self.load_cars(source_file)
 
 
     def load_cars(self, datafile):
         
         # Open datafile 
-        with open(datafile) as file:
-            pass
-
+        with open(datafile, 'r') as file:
+            cars = {}
+            reader = csv.DictReader(in_file)
+            for row in reader:
+                cars[row['name']]
 
     
