1c0d208016307f891195769f655d32dc839f985f
jazzlikefruit <shewen_dave@hotmail.com>
2020-06-04 21:34:44 +0200

    added board.py

---
+++ code/classes/board.py
@@ -0,0 +1,13 @@
+class Board():
+    def __init__(self, game):
+        self.cars = {}
+
+
+    def load_cars(self, datafile):
+        
+        # Open datafile 
+        with open(datafile) as file:
+            pass
+
+
+    
