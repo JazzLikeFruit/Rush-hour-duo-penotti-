73583c8f20ff5caedc9c555b0c008293df25eb33
jazzlikefruit <shewen_dave@hotmail.com>
2020-06-13 09:51:07 +0200

    checked move()

--- code/algorithms/random_algorithm.py
+++ code/algorithms/random_algorithm.py
@@ -34,7 +34,10 @@ def randy(inst, cars):
 
             # Reload board
             empty_board = instance_copy.create_board()
-            instance_copy.load_board(empty_board)
+            print(instance_copy.load_board(empty_board))
+            break
+        print("movenot possible")
+        break
 
     instance_copy.car_output()
     empty_board = instance_copy.create_board()
