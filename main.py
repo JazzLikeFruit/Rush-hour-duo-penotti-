6fb2c5119616afe8f82c7e213347bd246a98707b
jazzlikefruit <shewen_dave@hotmail.com>
2020-06-18 07:43:15 +0200

    added breadth first algorithm

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from code.algorithms import random_algorithm, unique_moves, short_path, end_point
+from code.algorithms import random_algorithm, unique_moves, short_path, end_point, breadth_first
 from code.classes import cars, board
 from numpy import random
 import csv
@@ -8,24 +8,26 @@ if __name__ == '__main__':
     instance = board.Board(datafile)
     empty_board = instance.create_board()
     cardic = instance.load_cars(datafile)
-    #records = []
+    records = []
     print("\nRUSH HOUR - Duo Penotti\n")
     print(instance.load_board(empty_board))
     print("\n====================================\n")
     print("Choose an alogithm to solve the puzzel with by typing the number :")
-    print("- 1 Random Algorithm\n- 2 Unique moves Algorithm\n- 3 Optimalised moves Algorithm\n- 4 End Point Algorithm")
-    algorithms={'1':'Random Algorithm', '2':'Unique moves Algorithm', '3':'Optimalised moves Algorithm', '4':'End Point Algorithm', '5':'Sample of all algorithms'}
+    algorithms = {'1': 'Random Algorithm', '2': 'Unique moves Algorithm',
+                  '3': 'Optimalised moves Algorithm', '4': 'End Point Algorithm', '5': 'Sample of all algorithms', '6': 'Beadth first algorithm'}
+    for algorithm in algorithms:
+        print(f"- {algorithm} {algorithms[algorithm]}")
+
     while True:
+
         print("\nEnter your choice:")
-        inputalgorithm=input().lower()
+        inputalgorithm = input().lower()
         if inputalgorithm not in algorithms:
-            print ('Incorrect algorithm select one of the following: ')
-            for algo in algorithms:
-                print (algo)
+            print('Incorrect algorithm select one of the following: ')
+
         else:
             print('Loading', algorithms[inputalgorithm], '...')
             break
-
 
     if inputalgorithm == '1':
         random_algorithm.randy(instance, cardic)
@@ -36,5 +38,8 @@ if __name__ == '__main__':
     elif inputalgorithm == '4':
         end_point.End_point(instance, cardic)
     elif inputalgorithm == '5':
-        #optie om alle algoritmes x aantal keer te laten draaien en de data te verzmalen in een diagram / csv
-        pass  
+        # optie om alle algoritmes x aantal keer te laten draaien en de data te verzmalen in een diagram / csv
+        pass
+    elif inputalgorithm == "6":
+        run = breadth_first.Breathfirst(instance, cardic)
+        run.run()
